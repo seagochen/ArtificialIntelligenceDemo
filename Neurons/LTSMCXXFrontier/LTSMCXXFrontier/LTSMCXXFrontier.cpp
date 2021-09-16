3dd267bde110a4ed6fd49e5028232471b05624c7
Orlando Chen <seagochen@hotmail.com>
2021-09-16 20:21:07 +0800

    新增C++运行端示例
    新增C++运行端示例

---
+++ Neurons/LTSMCXXFrontier/LTSMCXXFrontier/LTSMCXXFrontier.cpp
@@ -0,0 +1,9 @@
+﻿// LTSMCXXFrontier.cpp: 定义应用程序的入口点。
+//
+#include <torch/torch.h>
+#include <iostream>
+
+int main() {
+    torch::Tensor tensor = torch::eye(3);
+    std::cout << tensor << std::endl;
+}
